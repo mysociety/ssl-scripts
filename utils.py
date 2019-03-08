cf79f9cf5b584202f071eb48383e7631959ee444
Sam Pearson <sam@mysociety.org>
2019-03-08 16:58:12 +0000

    python3: Need to decode the JSON as UTF-8 in utils.py

--- utils.py
+++ utils.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import collections
 import json
@@ -70,7 +70,7 @@ class Vhosts(collections.Mapping):
     def _parse_vhosts_pl_section(self, vhosts_pl_path, section):
         return json.loads(subprocess.check_output([
             'perl', '-e', 'use JSON; require "' + vhosts_pl_path + '"; print encode_json($' + section + ');'
-        ]))
+        ]).decode('utf-8'))
 
     def _parse_vhosts_pl(self, vhosts_pl_path):
         vhosts = self._parse_vhosts_pl_section(vhosts_pl_path, 'vhosts')
